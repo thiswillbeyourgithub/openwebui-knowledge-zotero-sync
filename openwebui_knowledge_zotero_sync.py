931857cea9e82b689911c0cc797b79bb158ecfa2
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2026-01-25 19:36:06 +0100

    bump version 2.1.0 -> 2.2.0

--- openwebui_knowledge_zotero_sync.py
+++ openwebui_knowledge_zotero_sync.py
@@ -42,7 +42,7 @@ from tqdm import tqdm
 
 from utils.datatypes import File, KnowledgeBase, validate_response
 
-VERSION: str = "2.1.0"
+VERSION: str = "2.2.0"
 
 # Configure logger to write to both console and file
 # Detailed logs go to file, INFO+ goes to console
